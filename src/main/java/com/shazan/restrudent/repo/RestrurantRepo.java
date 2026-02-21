43081d1b6393cbbdbaf4a8e5868f53aebc7bf113
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-02-21 13:53:54 +0600

    security config added for keyloack

---
+++ src/main/java/com/shazan/restrudent/repo/RestrurantRepo.java
@@ -0,0 +1,11 @@
+package com.shazan.restrudent.repo;
+
+import com.shazan.restrudent.domain.entity.Restrurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestrurantRepo extends ElasticsearchRepository<Restrurant,Integer> {
+
+
+}
